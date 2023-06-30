d7e3ccdb952fd7be3343887eb4f580297c644d7c
AIE\s211426 <s211426@students.aie.edu.au>
2023-06-30 16:52:07 +0930

    Camera moves only on x-axis

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -12,11 +12,15 @@ public class CameraMovement : MonoBehaviour
     void Start()
     {
         _offset = transform.position - _target.position;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, _target.position + _offset, _followSpeed * Time.deltaTime);
+        Vector3 targetPosition = new Vector3(_target.position.x, 0, 0);
+        transform.position = Vector3.Lerp(transform.position, targetPosition + _offset, _followSpeed * Time.deltaTime);
+
+        //transform.position = Vector3.Lerp(transform.position, _target.position + _offset, _followSpeed * Time.deltaTime);
     }
 }
