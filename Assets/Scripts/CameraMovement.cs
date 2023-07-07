0fbc0c1e92574f0f49f376066fb4ca4f0d76150b
AIE\s211426 <s211426@students.aie.edu.au>
2023-07-07 11:17:37 +0930

    Wall detection to freeze camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,6 +21,43 @@ public class CameraMovement : MonoBehaviour
         Vector3 targetPosition = new Vector3(_target.position.x, 0, 0);
         transform.position = Vector3.Lerp(transform.position, targetPosition + _offset, _followSpeed * Time.deltaTime);
 
-        //transform.position = Vector3.Lerp(transform.position, _target.position + _offset, _followSpeed * Time.deltaTime);
+
+    }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float _followSpeed;
+    private Vector3 _offset;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _maxDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - _target.position;
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = new Vector3(_target.position.x, 0, 0);
+        RaycastHit hit;
+
+        if (Physics.Raycast(_target.position, _target.position, out hit, _maxDistance))
+        {
+            // Wall detected within maxDistance
+            // Stop camera movement here
+            transform.position = Vector3.Lerp(transform.position, targetPosition + _offset, 0);
+        }
+        else
+        {
+            // No wall detected within maxDistance
+            // Allow camera movement here
+            transform.position = Vector3.Lerp(transform.position, targetPosition + _offset, _followSpeed * Time.deltaTime);
+        }
     }
 }
