d25591141c74399a4df74b455ae7042db4868b71
AIE\s211426 <s211426@students.aie.edu.au>
2023-07-07 11:42:13 +0930

    changed wall detection to detect nly objects layered as wall

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,38 +1,13 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraMovement : MonoBehaviour
-{
-    [SerializeField] private float _followSpeed;
-    private Vector3 _offset;
-    [SerializeField] private Transform _target;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _offset = transform.position - _target.position;
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Vector3 targetPosition = new Vector3(_target.position.x, 0, 0);
-        transform.position = Vector3.Lerp(transform.position, targetPosition + _offset, _followSpeed * Time.deltaTime);
-
-
-    }
-}*/
-
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private float _followSpeed;
     private Vector3 _offset;
+    [SerializeField] private LayerMask _wall;
     [SerializeField] private Transform _target;
     [SerializeField] private float _maxDistance;
 
@@ -47,7 +22,7 @@ public class CameraMovement : MonoBehaviour
         Vector3 targetPosition = new Vector3(_target.position.x, 0, 0);
         RaycastHit hit;
 
-        if (Physics.Raycast(_target.position, _target.position, out hit, _maxDistance))
+        if (Physics.Raycast(_target.position, _target.position, out hit, _maxDistance, _wall))
         {
             // Wall detected within maxDistance
             // Stop camera movement here
