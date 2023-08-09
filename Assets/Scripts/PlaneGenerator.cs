9f016ad3bfa647c40a06c1599dfb1bc6253ea184
AIE\s211426 <s211426@students.aie.edu.au>
2023-08-09 16:49:18 +0930

    WorldGeneration to set size

--- Assets/Scripts/PlaneGenerator.cs
+++ Assets/Scripts/PlaneGenerator.cs
@@ -10,7 +10,7 @@ public class PlaneGenerator : MonoBehaviour
     [SerializeField] private int radius;
     [SerializeField] private int planeOffset;
     [SerializeField] private GameObject campgroundPrefab; // assign the campground prefab in the inspector
-    [SerializeField] private GameObject tree;
+    [SerializeField] private GameObject tree; // assign tree prefab in the inspector
 
     private Vector3 playerPrevPos = Vector3.zero;
     private Vector3 playerDeltaMove = Vector3.zero;
@@ -68,35 +68,35 @@ public class PlaneGenerator : MonoBehaviour
             started = false;
         }
 
-        if (hasPlayerMoved(_player.transform.position.x, _player.transform.position.z))
-        {
-            for (int x = -radius; x <= radius; x++)
-            {
-                for (int z = -radius; z <= radius; z++)
-                {
-                    Vector3 pos = new Vector3((x * planeOffset + XPlayerLocation), 0, (z * planeOffset + ZPlayerLocation));
+        //if (hasPlayerMoved(_player.transform.position.x, _player.transform.position.z))
+        //{
+        //    for (int x = -radius; x <= radius; x++)
+        //    {
+        //        for (int z = -radius; z <= radius; z++)
+        //        {
+        //            Vector3 pos = new Vector3((x * planeOffset + XPlayerLocation), 0, (z * planeOffset + ZPlayerLocation));
 
-                    if (!tilePlane.Contains(pos))
-                    {
-                        GameObject tile = Instantiate(plane, pos, Quaternion.identity, gameObject.transform);
-                        tilePlane.Add(pos, tile);
+        //            if (!tilePlane.Contains(pos))
+        //            {
+        //                GameObject tile = Instantiate(plane, pos, Quaternion.identity, gameObject.transform);
+        //                tilePlane.Add(pos, tile);
 
-                        // check if a campground should be spawned at this position
-                        if (ShouldSpawnCampground())
-                        {
-                            Instantiate(campgroundPrefab, pos, Quaternion.identity);
-                        }
-                        if (ShouldSpawnTree())
-                        {
-                            Instantiate(tree, pos, Quaternion.Euler(-90, 0, 0));
-                        }
-                    }
-                }
-            }
-        }
+        //                // check if a campground should be spawned at this position
+        //                if (ShouldSpawnCampground())
+        //                {
+        //                    Instantiate(campgroundPrefab, pos, Quaternion.identity);
+        //                }
+        //                if (ShouldSpawnTree())
+        //                {
+        //                    Instantiate(tree, pos, Quaternion.Euler(-90, 0, 0));
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
 
-        playerPrevPos.x = _player.transform.position.x;
-        playerPrevPos.z = _player.transform.position.z;
+        //playerPrevPos.x = _player.transform.position.x;
+        //playerPrevPos.z = _player.transform.position.z;
     }
 
     private bool ShouldSpawnCampground()
@@ -111,12 +111,12 @@ public class PlaneGenerator : MonoBehaviour
     }
 
 
-    private bool hasPlayerMoved(float playerX, float playerZ)
-    {
-        if (Mathf.Abs(playerX) >= planeOffset || Mathf.Abs(playerZ) >= planeOffset)
-        {
-            return true;
-        }
-        return false;
-    }
+    //private bool hasPlayerMoved(float playerX, float playerZ)
+    //{
+    //    if (Mathf.Abs(playerX) >= planeOffset || Mathf.Abs(playerZ) >= planeOffset)
+    //    {
+    //        return true;
+    //    }
+    //    return false;
+    //}
 }
