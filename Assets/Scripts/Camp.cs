3e5d53a0865de01349b27d72dff8ff7d80e7a29c
DESKTOP-1KV0JGV\Jaxon <s220465@students.aie.edu.au>
2023-08-12 10:42:01 +0930

    comments

--- Assets/Scripts/Camp.cs
+++ Assets/Scripts/Camp.cs
@@ -19,16 +19,23 @@ public class Camp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // random number between 5 and 10 to determin how many enemys spawn in camp
         _numberOfEnemys = Random.Range(5,10);
 
+        // for loop goes and spawns the number of enemys
         for (int i = 0; i < _numberOfEnemys; i++)
         {
+            // This number will determin which enemy will spawn from the _enemy array
             _enemyNumber = Random.Range(0, _enemys.Length);
+            // 1-3 knike guy, 4,5 spear guy and 6 is sniper guy
             _enemy = _enemys[_enemyNumber];
+            //random x,y position inside the camps range
             _xLocation = transform.position.x + Random.Range(-6,6);
             _zLocation = transform.position.z + Random.Range(-6,6);
+            // make _enemy location = to the random location of x and y
             _enemyLocation = new Vector3(_xLocation, 2, _zLocation);
 
+            // spawn enemy at that location
             Instantiate(_enemy, _enemyLocation, Quaternion.identity, transform);
         }
     }
